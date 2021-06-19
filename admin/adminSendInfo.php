1e65e9e7d86110c678036c2da69ae24c7a4ccea7
Razvan Marius <pojar.razvanmarius@gmail.com>
2021-06-19 16:21:57 +0300

    Solved problem admin

--- admin/adminSendInfo.php
+++ admin/adminSendInfo.php
@@ -2,32 +2,28 @@
 <?php include '../db_conn.php' ?>
 
 <?php
-if (isset($_SESSION['admin_id']) && isset($_SESSION['admin_email'])) {
 
 
-    if (isset($_POST['submit'])) {
 
-        $email = $_POST['email_user'];
-        $tip = $_POST['tip'];
-        $nume = $_POST['nume'];
-        $repetari = $_POST['repetari'];
-        $pauza = $_POST['pauza'];
-        $descriere = $_POST['descriere'];
-        $link = $_POST['link'];
+if (isset($_POST['submit'])) {
 
-        $sql = "INSERT INTO personalized_training (email_user, tip, nume, repetari, pauza, descriere, link)
+    $email = $_POST['email_user'];
+    $tip = $_POST['tip'];
+    $nume = $_POST['nume'];
+    $repetari = $_POST['repetari'];
+    $pauza = $_POST['pauza'];
+    $descriere = $_POST['descriere'];
+    $link = $_POST['link'];
+
+    $sql = "INSERT INTO personalized_training (email_user, tip, nume, repetari, pauza, descriere, link)
         VALUES ('$email', '$tip', '$nume', '$repetari','$pauza','$descriere','$link')";
-        $result = $conn->query($sql);
-        if ($result) {
-            header("Location: admin/adminindex.php");
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->errorInfo();
-        }
+    $result = $conn->query($sql);
+    if ($result) {
+        header("Location: admin/adminindex.php");
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->errorInfo();
     }
+}
 ?>
 
 
-
-<?php } else {
-    header("Location: adminLogin.php");
-} ?>
