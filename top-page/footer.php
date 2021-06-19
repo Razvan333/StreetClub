a837b2fbe70109a767c6e0fee24b138756df5890
Razvan Marius <pojar.razvanmarius@gmail.com>
2021-06-19 16:57:54 +0300

    change the place of the chat

--- top-page/footer.php
+++ top-page/footer.php
@@ -5,6 +5,35 @@
     <link rel="stylesheet" href="../style.css">
 </head>
 
+<body>
+    <!-- Messenger Plugin pentru chat Code -->
+    <div id="fb-root"></div>
+
+    <!-- Your Plugin pentru chat code -->
+    <div id="fb-customer-chat" class="fb-customerchat">
+    </div>
+
+    <script>
+        var chatbox = document.getElementById('fb-customer-chat');
+        chatbox.setAttribute("page_id", "100437712265408");
+        chatbox.setAttribute("attribution", "biz_inbox");
+        window.fbAsyncInit = function() {
+            FB.init({
+                xfbml: true,
+                version: 'v10.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
+</body>
 <!--<div class="img-fluid bg-footer w-100 max-width: 100%;height: auto">-->
 <footer id="contact">
     <div class="container-fluid" style="background-color:black">
